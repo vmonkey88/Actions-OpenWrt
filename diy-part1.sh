d5cb5ec57e5658794d3d62c5701152b197a003b1
vmonkey88 <65351361+vmonkey88@users.noreply.github.com>
2021-04-25 10:31:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb package/jerrykuku
-git clone https://github.com/jerrykuku/luci-app-vssr package/jerry
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/rfs
-git clone https://github.com/rufengsuixing/luci-app-autoipsetadder package/rfss
-# fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+git clone https://github.com/kenzok8/openwrt-packages package/ken
+git clone https://github.com/kenzok8/small package/ken1
